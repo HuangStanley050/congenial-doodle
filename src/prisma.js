a3e1ab528481e93dca61052bfc73bc7e370e71b0
Stanley Huang <huang.stanley050@gmail.com>
2019-12-01 14:41:35 +1100

    got nodejs to work with prisma-binding

--- src/prisma.js
+++ src/prisma.js
@@ -2,6 +2,12 @@ import { Prisma } from "prisma-binding";
 
 const prisma = new Prisma({
   typeDefs: "src/generated/prisma.graphql",
-  endpoint: "localhost:4466",
+  endpoint: "http://localhost:4466",
   secret: ""
 });
+
+prisma.query
+  .users(null, "{name email id posts {id title body}}")
+  .then(users => {
+    console.log(JSON.stringify(users, undefined, 2));
+  });
