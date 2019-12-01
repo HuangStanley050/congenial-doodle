2b29e3949a7ae8af563e1853234d2036f8c5b970
Stanley Huang <huang.stanley050@gmail.com>
2019-12-01 19:25:00 +1100

    getting more practice with mutation plus query in promise chain

--- src/prisma.js
+++ src/prisma.js
@@ -6,8 +6,63 @@ const prisma = new Prisma({
   secret: ""
 });
 
-prisma.query
-  .users(null, "{name email id posts {id title body}}")
-  .then(users => {
-    console.log(JSON.stringify(users, undefined, 2));
-  });
+// prisma.query
+//   .users(null, "{name email id posts {id title body}}")
+//   .then(users => {
+//     console.log(JSON.stringify(users, undefined, 2));
+//   });
+//
+// prisma.query
+//   .comments(null, "{text author {id name}}")
+//   .then(comments => console.log(JSON.stringify(comments, undefined, 2)));
+//
+
+// prisma.mutation
+//   .createPost(
+//     {
+//       data: {
+//         title: "Booooaaaa",
+//         body: "Mery xmas",
+//         published: true,
+//         author: {
+//           connect: {
+//             email: "test3@test.com"
+//           }
+//         }
+//       }
+//     },
+//     "{id title body published}"
+//   )
+//   .then(post => {
+//     console.log(JSON.stringify(post, undefined, 2));
+//     return prisma.query.users(null, "{id name email posts { id title body }}");
+//   })
+//   .then(users => console.log(JSON.stringify(users, undefined, 2)));
+
+prisma.mutation
+  .updatePost(
+    {
+      data: {
+        published: false,
+        title: "This is another challenge",
+        body: "I am going to bed",
+        author: {
+          connect: {
+            email: "test3@test.com"
+          }
+        }
+      },
+      where: {
+        id: "ck3mq9n6p00520738ooco2ab7"
+      }
+    },
+    "{id title body published author {id name email}}"
+  )
+  .then(post => {
+    console.log(JSON.stringify(post, undefined, 2));
+    return prisma.query.posts(
+      null,
+      "{id title body published author {id name email}}"
+    );
+  })
+  .then(posts => console.log(JSON.stringify(posts, undefined, 2)));
