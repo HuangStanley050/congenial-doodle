835b405b8478b06a03dc68b2cbda607a4798f5cb
Stanley Huang <huang.stanley050@gmail.com>
2019-12-10 20:13:02 +1100

    added new subscription for myPost

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,4 +1,22 @@
+import { getUserId } from "../utils/getUserId";
 const Subscription = {
+  myPost: {
+    subscribe(parent, args, { prism, req }, info) {
+      const userId = getUserId(req);
+      return prisma.subscription.post(
+        {
+          where: {
+            node: {
+              author: {
+                id: userId
+              }
+            }
+          }
+        },
+        info
+      );
+    }
+  },
   comment: {
     subscribe(parent, { postId }, { prisma }, info) {
       return prisma.subscription.comment(
--- src/utils/getUserId.js
+++ src/utils/getUserId.js
@@ -1,6 +1,8 @@
 import jwt from "jsonwebtoken";
 export const getUserId = (req, requireAuth = true) => {
-  const header = req.request.headers.authorization;
+  const header = req.request
+    ? req.request.headers.authorization
+    : req.connection.context.Authorization;
 
   if (header) {
     const token = header.replace("Bearer ", "");
